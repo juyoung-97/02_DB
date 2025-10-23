db8085680ac636bc1f18940abed8aedea8fa4c48
juyoung-97 <boduni20@gmail.com>
2025-10-23 14:50:34 +0900

    WORKBOOK

--- workbook_FUNCTION.sql
+++ workbook_FUNCTION.sql
@@ -24,7 +24,7 @@ WHERE PROFESSOR_NAME NOT LIKE '___';
 -- 나이는 '만'으로 계산한다
 
 SELECT PROFESSOR_NAME AS 교수이름, 
-FLOOR((MONTHS_BETWEEN(SYSDATE,  TO_DATE('19'||SUBSTR(PROFESSOR_SSN, 1, 6), 'RRMMDD')) / 12)) AS 나이
+FLOOR((MONTHS_BETWEEN(SYSDATE,  TO_DATE('19'||SUBSTR(PROFESSOR_SSN, 1, 6), 'YYYYMMDD')) / 12)) AS 나이
 FROM TB_PROFESSOR
 WHERE SUBSTR(PROFESSOR_SSN, 8, 1) = '1'
 ORDER BY 나이;
@@ -32,13 +32,83 @@ ORDER BY 나이;
 -- 4. 교수들의 이름 중 성을 제외한 이름만 출력하는 SQL 문장 작성 
 -- 출력 헤더는 "이름"이 찍히도록한다 (성이 2자인 경우의 교수는 없다고 가정)
 
-SELECT SUBSTR(PROFESSOR_NAME, 2, 3)
+SELECT SUBSTR(PROFESSOR_NAME, 2, 3) 이름
 FROM TB_PROFESSOR
 
 -- 5. 춘 기술대학교의 재수생 입학자를 구하려고 한다. 어떻게 찾아낼 것인가?
 -- 이때, 19살에 입학하면 재수를 하지 않은 것으로 간주한다
+SELECT * FROM TB_STUDENT ts ;
+SELECT STUDENT_NO, STUDENT_NAME
+FROM TB_STUDENT
+WHERE EXTRACT(YEAR FROM ENTRANCE_DATE) - 
+EXTRACT(YEAR FROM TO_DATE(SUBSTR(STUDENT_SSN,1,6),'RRMMDD')) > '19';
+
+-- 6. 2020년 크리스마스는 무슨 요일인가? 금요일
+
+SELECT TO_CHAR(TO_DATE('2020-12-25', 'YYYY-MM-DD'), 'Day')
+FROM DUAL;
+
+-- 7. TO_DATE('99/10/11','YY/MM/DD'), TO_DATE('49/10/11','YY/MM/DD') 은 각각
+--  몇 년 몇 월 몇 일을 의미할까? 또 TO_DATE('99/10/11','RR/MM/DD')
+-- TO_DATE('49/10/11','RR/MM/DD') 은 각각 몇 년 몇 월 몇 일을 의미할까?
+SELECT TO_DATE('99/10/11','YY/MM/DD') FROM DUAL; -- 2099-10-11 00:00:00.0|00
+SELECT TO_DATE('49/10/11','YY/MM/DD') FROM DUAL; -- 2049-10-11 00:00:00.000
+SELECT TO_DATE('99/10/11','RR/MM/DD') FROM DUAL; -- 1999-10-11 00:00:00.000
+SELECT TO_DATE('49/10/11','RR/MM/DD') FROM DUAL; -- 2049-10-11 00:00:00.000
+-- R 타입은 1950 이전은 2000년대로 해석함
+
+-- 8. 춘 기술대학교의 2000 년도 이후 입학자들은 학번이 A 로 시작하게 되어있다. 2000 년도 이전 학번을 받은
+-- 학생들의 학번과 이름을 보여주는 SQL 문장을 작성하시오
+
+SELECT STUDENT_NO, STUDENT_NAME
+FROM TB_STUDENT
+WHERE STUDENT_NO NOT LIKE 'A%';
+
+-- 9. 학번이 A517178 인 한아름 학생의 학점 총 평점을 구하는 SQL 문을 작성하시오.
+-- 단, 이때 출력 화면의 헤더는 "평점" 이라고 찍히게 하고, 점수는 반올림하여 소수점 이하 한자리까지만 표시한다
+
+SELECT * FROM TB_GRADE;
+SELECT ROUND(AVG(POINT),1)  "총 평점"
+FROM TB_GRADE
+WHERE STUDENT_NO = 'A517178';
+
+-- 10.학과별 학생수를 구하여 "학과번호", "학생수(명)" 의 형태로 헤더를 만들어 결과값이 출력되도록 하시오.
+
+SELECT * FROM TB_STUDENT;
+SELECT DEPARTMENT_NO "학과번호", COUNT(DEPARTMENT_NO) "학생수(명)"
+FROM TB_STUDENT
+GROUP BY DEPARTMENT_NO
+ORDER BY DEPARTMENT_NO;
+
+-- 11. 지도 교수를 배정받지 못한 학생 수는 몇 명 정도 되는 지를 알아내는 SQL 문을 작성하시오
+
+SELECT COUNT(*)
+FROM TB_STUDENT
+WHERE COACH_PROFESSOR_NO IS NULL;
+
+-- 12. 학번이 A112113인 김고운 학생의 년도 별 평점을 구하는 SQL 문을 작성하시오. 
+-- 단, 이때 출력 화면의 헤더는 "년도", "년도 별 평점" 이라고 찍히게 하고 점수는 반올림하여 소수점 이하 한자리 까지만 표시
+SELECT * FROM TB_GRADE; 
+
+SELECT SUBSTR(TERM_NO, 1, 4) "년도", ROUND(AVG(POINT),1) "년도 별 평점"
+FROM TB_GRADE
+WHERE STUDENT_NO = 'A112113'
+GROUP BY SUBSTR(TERM_NO, 1, 4)
+ORDER BY SUBSTR(TERM_NO, 1, 4);
+
+-- 13. 학과 별 휴학생 수를 파악하고자 한다. 학과 번호와 휴학생 수를 표시하는 SQL 문장을 작성하시오
+
+SELECT DEPARTMENT_NO, COUNT(*)
+FROM TB_STUDENT
+WHERE ABSENCE_YN = 'Y'
+GROUP BY DEPARTMENT_NO
+ORDER BY DEPARTMENT_NO;
 
 
+-- 14. 춘 대학교에 다니는 동명이인 학생들의 이름을 찾고자 한다 어떤 SQL 문장을 사용하면 가능하겠는가?
+
+-- 15. 학번이 A112113 인 김고운 학생의 년도, 학기 별 평점과 년도 별 누적평점, 총평점을 구하는
+-- SQL 문을 작성하시오 (단, 평점은 소수점 1자리까지만 반올리하여 표시한다)
 
 
 
