2932edf676e29a8b28c3f4693d5a5492da5ab731
juyoung-97 <boduni20@gmail.com>
2025-10-24 16:33:04 +0900

    WORKBOOK

--- 사용자계정생성Script.sql
+++ 사용자계정생성Script.sql
@@ -17,14 +17,14 @@
 ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 -- 11G 이전 문법 사용 허용
 
-CREATE USER workbook IDENTIFIED BY workbook;
+CREATE USER kh_shop IDENTIFIED BY kh1234;
 -- 계정 생성 구문 (kh_jjy : USERNAME / kh1234 : Password)
 
-GRANT RESOURCE, CONNECT TO workbook;
+GRANT RESOURCE, CONNECT TO kh_shop;
 -- 사용자 계정에 권한 부여 설정
 -- RESOURCE : 테이블이나 인덱스 같은 DB 객체를 생성할 권한
 -- CONNECT : DB 에 연결하고 로그인 할 수 있는 권한
 
-ALTER USER workbook DEFAULT TABLESPACE SYSTEM QUOTA
+ALTER USER kh_shop DEFAULT TABLESPACE SYSTEM QUOTA
 UNLIMITED ON SYSTEM;
 -- 객체가 생성될 수 있는 공간 할당량 무제한 지정
